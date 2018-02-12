f4f89b609183ef6f524a76a06ae84587f3484696
PCE <pete.earle@gmail.com>
2018-02-12 13:38:30 -0500

    Update Crypto.java

--- Crypto.java
+++ Crypto.java
@@ -14,22 +14,30 @@ public class Crypto {
      */
     public static boolean verifySignature(PublicKey pubKey, byte[] message, byte[] signature) {
         Signature sig = null;
+
+        // use RSA signature
         try {
             sig = Signature.getInstance("SHA256withRSA");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
+
+        // initialize with input publicKey
         try {
             sig.initVerify(pubKey);
         } catch (InvalidKeyException e) {
             e.printStackTrace();
         }
+
+        // update with message and verify the signature
         try {
             sig.update(message);
             return sig.verify(signature);
         } catch (SignatureException e) {
             e.printStackTrace();
         }
+
+        // default return false
         return false;
 
     }
